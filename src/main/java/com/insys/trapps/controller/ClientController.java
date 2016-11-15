cc6cfd8a1ede15c3d09a1038f95ee2907ef924d7
Kshitiz 'Kris' Krishna <kkrishnainsys.com@Kshitizs-MacBook-Pro.local>
2016-11-15 06:52:39 -0700

    Basic create client

--- src/main/java/com/insys/trapps/controller/ClientController.java
+++ src/main/java/com/insys/trapps/controller/ClientController.java
@@ -3,17 +3,32 @@ package com.insys.trapps.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insys.trapps.model.Client;
+import com.insys.trapps.respositories.ClientRepository;
+
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
 	
+	 @Autowired
+	 ClientRepository repository;
+	
 	@RequestMapping("/")
-	public List<Client> listClients() {
-		return Arrays.asList(new Client[]{new Client("Test Client")});
+	public List<Iterable<Client>> listClients() {
+		return Arrays.asList(repository.findAll());
+	}
+	
+	@RequestMapping("/create")
+	public Client createClient() {
+		return repository.save(new Client(
+				          Long.valueOf(1), "test", "testing", "denver", 
+				         "MZ", "303-2682717", new String[]{"kkrishna@insys.com"}, "Insys-drive", 
+				         new String[]{"personnel-srvc"}, new String[]{"project-1"}, true, "2017"));
+				
 	}
 }
--- src/main/java/com/insys/trapps/model/Client.java
+++ src/main/java/com/insys/trapps/model/Client.java
@@ -1,25 +1,223 @@
 package com.insys.trapps.model;
 
-public class Client {
-	private String name;
+import java.util.Arrays;
 
-	public Client(String name) {
-		this.name=name;
-	}
+import javax.persistence.*;;
+
+@Entity
+@Table(name = "client")
+public class Client {
 	
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String region;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String[] emails;
+
+    @Column(nullable = false)
+    private String address;
+
+  
+    @Column(nullable = false)
+    private String[] projects;
+
+    @Column(nullable = false)
+    private String[] roles;
+
+    @Column(nullable = false)
+    private boolean msa;
+   
+    @Column(nullable = false)
+    private String msaExpirationDate;
+    
+    
+    
+    
+	public Client() {
+		super();
+	}
+
+
+
+	public Client(Long id, String name, String description, String location,
+			String region, String phone, String[] emails, String address,
+			String[] projects, String[] roles, boolean msa, String msaExpirationDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.location = location;
+		this.region = region;
+		this.phone = phone;
+		this.emails = emails;
+		this.address = address;
+		this.projects = projects;
+		this.roles = roles;
+		this.msa = msa;
+		this.msaExpirationDate = msaExpirationDate;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
 	public String getName() {
 		return name;
 	}
+
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	
-	@Override
-	public String toString() {
-		return "Client [name=" + name + "]";
+
+
+	public String getDescription() {
+		return description;
 	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+
+	public String getRegion() {
+		return region;
+	}
+
+
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+	public String[] getEmails() {
+		return emails;
+	}
+
+
+
+	public void setEmails(String[] emails) {
+		this.emails = emails;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String[] getProjects() {
+		return projects;
+	}
+
+
+
+	public void setProjects(String[] projects) {
+		this.projects = projects;
+	}
+
+
+
+	public String[] getRoles() {
+		return roles;
+	}
+
+
+
+	public void setRoles(String[] roles) {
+		this.roles = roles;
+	}
+
+
+
+	public boolean isMsa() {
+		return msa;
+	}
+
+
+
+	public void setMsa(boolean msa) {
+		this.msa = msa;
+	}
+
+
+
+	public String getMsaExpirationDate() {
+		return msaExpirationDate;
+	}
+
+
+
+	public void setMsaExpirationDate(String msaExpirationDate) {
+		this.msaExpirationDate = msaExpirationDate;
+	}
+
+
 
 	@Override
 	public int hashCode() {
@@ -45,6 +243,20 @@ public class Client {
 			return false;
 		return true;
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", description=" + description
+				+ ", location=" + location + ", region=" + region + ", phone=" + phone
+				+ ", emails=" + Arrays.toString(emails) + ", address=" + address
+				+ ", projects=" + Arrays.toString(projects) + ", roles="
+				+ Arrays.toString(roles) + ", msa=" + msa + ", msaExpirationDate="
+				+ msaExpirationDate + "]";
+	}
+	
+	
 	
 	
 }
