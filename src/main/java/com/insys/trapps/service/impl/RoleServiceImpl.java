0482695261d585d7847225da6b1dd2862fd5bc18
Rohit <1990.rohitnarwal@gmail.com>
2016-11-22 15:26:30 -0500

    TRAP-66: Added RoleServiceImpl functionality

--- src/main/java/com/insys/trapps/service/impl/RoleServiceImpl.java
+++ src/main/java/com/insys/trapps/service/impl/RoleServiceImpl.java
@@ -1,21 +1,15 @@
 package com.insys.trapps.service.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
 
-import com.insys.trapps.model.Business;
 import com.insys.trapps.model.Role;
-import com.insys.trapps.respositories.BusinessRepository;
 import com.insys.trapps.respositories.RoleRepository;
-import com.insys.trapps.service.BusinessService;
 import com.insys.trapps.service.RoleService;
 
 @Component("roleService")
@@ -25,7 +19,15 @@ public class RoleServiceImpl implements RoleService {
 	RoleRepository roleRepository;
 
 	public List<Role> listRoles() {
-		return null;
+		List<Role> roleCollection = new ArrayList<>();
+		roleRepository.findAll().forEach(roleCollection::add);
+		return roleCollection;
+	}
+
+	public Role createRole(Role role) {
+		Assert.notNull(role, "Business must not be null");
+		Role savedRole = roleRepository.save(role);
+		return savedRole;
 	}
 
 }
