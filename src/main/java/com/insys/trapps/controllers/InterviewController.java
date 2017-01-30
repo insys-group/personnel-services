2fa8fe6b747536928c6a0a08f5ece582d9867331
Mikhail Tian <mtian@insys.com>
2017-01-30 16:48:57 -0500

    TRAP-105 Fixed intergration tests and domain model

--- src/main/java/com/insys/trapps/controllers/InterviewController.java
+++ src/main/java/com/insys/trapps/controllers/InterviewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.insys.trapps.model.interview.Interview;
@@ -32,7 +33,8 @@ public class InterviewController {
 	
 	@GetMapping(value = "/interviews/{id}") 
 	@ResponseStatus(HttpStatus.OK)
-	public Interview getInterview(@PathVariable("id") Long id) {
+	@ResponseBody
+	public  Interview getInterview(@PathVariable("id") Long id) {
 		return service.getInterview(id);
 	}
 }
--- src/test/java/com/insys/trapps/integration/InterviewRestIntegrationTest.java
+++ src/test/java/com/insys/trapps/integration/InterviewRestIntegrationTest.java
@@ -1,16 +1,20 @@
 package com.insys.trapps.integration;
 
 import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,17 +33,17 @@ import com.insys.trapps.model.PersonType;
 import com.insys.trapps.model.Role;
 import com.insys.trapps.model.interview.Feedback;
 import com.insys.trapps.model.interview.Interview;
+import com.insys.trapps.model.interview.Interview.InterviewBuilder;
 import com.insys.trapps.model.interview.Quality;
 import com.insys.trapps.model.interview.Question;
 import com.insys.trapps.respositories.BusinessRepository;
 import com.insys.trapps.respositories.PersonRepository;
 import com.insys.trapps.respositories.RoleRepository;
 import com.insys.trapps.respositories.interview.FeedbackRepository;
-import com.insys.trapps.respositories.interview.InterviewRepository;
 import com.insys.trapps.util.BusinessBuilder;
 import com.insys.trapps.util.RoleBuilder;
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
+import com.jayway.restassured.path.json.JsonPath;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -47,8 +51,6 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
 public class InterviewRestIntegrationTest {
-	@Autowired
-	private InterviewRepository interviewRepo;
 	@Autowired
 	private FeedbackRepository feedbackRepo;
 	@Autowired
@@ -64,182 +66,173 @@ public class InterviewRestIntegrationTest {
 	@Value("${spring.data.rest.basePath}")
 	private String basePath;
 
-	private final String INT_PATH = "/interviews";
-	
-	private Business mockBusiness;
+	private final String INTERVIEW_PATH = "/interviews";
 
-	private Interview mockInterview;
-	private Long mockId = 12345L;
-	private Feedback mockFeedback;
-	private Set<Person> mockInterviewers;
-	private Set<Question> mockQuestions;
-	private Person mockCandidate;
-	private long mockDate;
-	private Role mockRole;
-	
-	private Person employee0, employee1, interviewer;
-	
+	private Business business;
+
+	private Feedback feedback;
+	private Role role;
+
+	private Person firstInterviewer, secondInterviewer, candidate, updatedCandidate;
+
 	@Before
 	public void setup() {
 		RestAssured.port = port;
-		
-		if (mockBusiness == null) {
-			mockBusiness = BusinessBuilder.buildBusiness("Insys", "Interview", BusinessType.INSYS).build();
-			businessRepo.saveAndFlush(mockBusiness);
-		}
-		
-		if (mockDate == 0) {
-			mockDate = new Date().getTime();
-		}
-		
-		if (mockRole == null) {
-			mockRole = RoleBuilder.buildRole("Swift Developer II").build();
-			roleRepo.saveAndFlush(mockRole);
-		}
-		
-		if (mockInterviewers == null) {
-			employee0 = buildPerson("Hung", "Do", "hdo@insys.com", "Architect", PersonType.Employee);
-			employee1 = buildPerson("Rohit", "Narwhal", "rfnu@insys.com", "Architect", PersonType.Employee);
-			mockInterviewers = new HashSet<Person>(Arrays.asList(employee0, employee1));
-		}
-		
-		if (mockCandidate == null) {
-			mockCandidate = buildPerson("Person A", "Last name", "Email", "Architect", PersonType.Candidate);
-			personRepo.saveAndFlush(mockCandidate);
-		}
 
-		if (mockQuestions == null) {
-			Question q0 = Question.builder().question("Question 1").build();
-			Question q1 = Question.builder().question("Question 2").build();
-			Question q2 = Question.builder().question("Question 3").build();
-	
-			mockQuestions = new HashSet<>(Arrays.asList(q0, q1, q2));
-		}
+		business = BusinessBuilder.buildBusiness("Insys", "Interview", BusinessType.INSYS).build();
+		businessRepo.saveAndFlush(business);
 
-		if (mockInterview == null) {
-			interviewer = buildPerson("Hung", "Do", "hdo@insys.com", "Architect", PersonType.Employee);
-			personRepo.saveAndFlush(interviewer);
-			
-			mockFeedback = Feedback.builder().comment("Excellent").interviewer(interviewer).build();
-			feedbackRepo.save(mockFeedback);
-			
-			mockInterview = createMockInterview();
-			mockInterview.setId(interviewRepo.saveAndFlush(mockInterview).getId());
-			log.debug(mockInterview.getId() + " mock");
-		}
-	}
+		role = RoleBuilder.buildRole("Swift Developer II").build();
+		roleRepo.saveAndFlush(role);
 
-	@After
-	public void cleanup() {
+		firstInterviewer = buildPerson("Hung", "Do", "hdo@insys.com", "Architect", PersonType.Employee);
+		personRepo.save(firstInterviewer);
+		secondInterviewer = buildPerson("Rohit", "Narwhal", "rfnu@insys.com", "Architect", PersonType.Employee);
+		personRepo.save(secondInterviewer);
+		candidate = buildPerson("Candidate First Name", "Candidate Last Name", "Email", "Architect",
+				PersonType.Candidate);
+		personRepo.save(candidate);
+		updatedCandidate = buildPerson("Updated First Name", "Updated Last Name", "updated@email.com", "Updated Title",
+				PersonType.Candidate);
+		personRepo.saveAndFlush(updatedCandidate);
+
+		feedback = Feedback.builder().comment("Excellent").interviewer(firstInterviewer).build();
+		feedbackRepo.save(feedback);
+
 	}
 
 	@Test
-	public void testCreateInterviewWithEmptyInterview() {
+	public void testCreateEmptyInterview() {
 		Interview interview = Interview.builder().build();
-		given().contentType("application/json").body(interview).log().everything()
-			.when().post(basePath + INT_PATH)
-			.then().statusCode(HttpStatus.CONFLICT.value());
+
+		postRequestForInterviewReturns(interview, HttpStatus.CONFLICT);
 	}
-	
+
 	@Test
 	public void testCreateInterviewWithBasicInfo() {
-		Interview interview = Interview.builder()
-				.candidate(mockCandidate)
-				.role(mockRole)
-				.date(mockDate).build();
-		given().contentType("application/json").body(interview).log().everything()
-			.when().post(basePath + INT_PATH)
-			.then().statusCode(HttpStatus.CREATED.value());
+		Interview interview = initBasicInterview();
+
+		int id = postRequestForInterview(interview).getInt("id");
+		JsonPath response = getRequestForInterview(id);
+
+		Person candidate = response.getObject("candidate", Person.class);
+		assertThat(candidate.getFirstName(), equalTo("Candidate First Name"));
+		assertThat(candidate.getLastName(), equalTo("Candidate Last Name"));
 	}
-	
+
 	@Test
 	public void testUpdateInterviewWithQuestions() {
-		Question q0 = createMockQuestion("Question 1");
-		Question q1 = createMockQuestion("Question 2");
-		Question q2 = createMockQuestion("Question 3");
-		
-		Interview interview = mockInterview;
-		log.debug(interview.getId() +" test");
-		
-		Map<String, Question>questionMap = new HashMap<>();
-		questionMap.put(q0.getQuestion(), q0);
-		questionMap.put(q1.getQuestion(), q1);
-		questionMap.put(q2.getQuestion(), q2);
-		
+		Interview interview = createInterview();
+
+		long id = postRequestForInterview(interview).getInt("id");
+		String[] questionNames = new String[] { "Updated Question 1", "Updated Question 2" };
+		setNewFieldsToInterview(interview, id, questionNames);
+		long updatedId = postRequestForInterview(interview).getInt("id");
+		JsonPath updatedInterview = getRequestForInterview(id);
+
+		assertThat(updatedId, equalTo(id));
+		checkPerson(updatedInterview);
+		checkQuestions(updatedInterview, questionNames);
+	}
+
+	@Test
+	public void testPatchInterviewWithQuestions() {
+		Interview interview = createInterview();
+		Map<String, Question> questionsPatch = createQuestions("Patched Question 1", "Patched Question 2").stream()
+				.collect(Collectors.toMap(Question::getQuestion, question -> question));
+		String questionsInJson = convertQuestionsToJson(questionsPatch);
+
+		long id = postRequestForInterview(interview).getInt("id");
+		patchRequestForInterview(id, questionsInJson);
+		JsonPath updatedInterview = getRequestForInterview(id);
+
+		checkQuestions(updatedInterview, "Patched Question 1", "Patched Question 2");
+	}
+
+	private void patchRequestForInterview(long id, String json) {
+		log.debug("PATCH REQUEST");
+		given().contentType("application/json").body("{\"questions\":" + json + "}").when().log().everything()
+				.patch(basePath + INTERVIEW_PATH + "/" + id).then().statusCode(HttpStatus.ACCEPTED.value()).extract()
+				.response();
+	}
+
+	private String convertQuestionsToJson(Map<String, Question> questionsPatch) {
 		String json = "";
 		try {
-			json = new ObjectMapper().writeValueAsString(questionMap);
+			json = new ObjectMapper().writeValueAsString(questionsPatch);
 		} catch (JsonProcessingException e) {
 			log.error(e.getMessage());
 		}
-		
-		// update using put
-		log.debug("PATCH REQUEST");
-		given()
-			.contentType("application/json")
-//			.body("{\"questions\":" + json + "}")
-			.body(interview)
-		.when()
-//			.patch(basePath + INT_PATH + "/" + interview.getId())
-			.post(basePath + INT_PATH)
-		.then()
-			.log().everything()
-//			.statusCode(HttpStatus.ACCEPTED.value())
-			.statusCode(HttpStatus.CREATED.value())
-			.extract().response();
-
-		// check if update took with get
-		log.debug("GET REQUEST");
-		Response response = 
-		given()
-			.contentType("application/json")
-		.when()
-			.get(basePath + INT_PATH + "/" + interview.getId())
-		.then()
-			.log().everything()
-			.statusCode(HttpStatus.OK.value())
-			.extract().response();
-		
-		log.debug(response.jsonPath().prettify());
-		
-		// List<Map<String, Object>> questions = response.jsonPath().getList("questions");
-		// assertEquals(3, questions.size());
-		
-//		questions.forEach(question -> {
-//			assertNotNull(question.get("id"));
-//			log.debug("Question : " + question.get("question"));
-//		});
+		return json;
 	}
 
-	@Test
-	public void testGetInterviews() {
-		when().get(basePath + INT_PATH).then().statusCode(HttpStatus.OK.value());
+	private void checkQuestions(JsonPath updatedInterview, String... questionNames) {
+		List<String> updatedQuestions = getQuestions(updatedInterview);
+		assertThat(updatedQuestions.size(), equalTo(questionNames.length));
+		assertThat(updatedQuestions, hasItems(questionNames));
+	}
+
+	private void checkPerson(JsonPath updatedInterview) {
+		Person updatedCandidate = updatedInterview.getObject("candidate", Person.class);
+		assertThat(updatedCandidate.getFirstName(), equalTo("Updated First Name"));
+		assertThat(updatedCandidate.getLastName(), equalTo("Updated Last Name"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<String> getQuestions(JsonPath updatedTraining) {
+		List<HashMap<String, Object>> tasks = (List<HashMap<String, Object>>) updatedTraining.get("questions");
+		return tasks.stream().map(map -> (String) map.get("question")).collect(Collectors.toList());
+	}
+
+	private void setNewFieldsToInterview(Interview interview, long id, String... questionNames) {
+		Set<Question> questions = createQuestions(questionNames);
+		interview.setId(id);
+		interview.setCandidate(updatedCandidate);
+		interview.setQuestions(questions);
+	}
+
+	private JsonPath postRequestForInterview(Interview interview) {
+		return postRequestForInterviewReturns(interview, HttpStatus.CREATED);
+	}
+
+	private JsonPath postRequestForInterviewReturns(Interview interview, HttpStatus status) {
+		return given().contentType("application/json").body(interview).log().everything().when()
+				.post(basePath + INTERVIEW_PATH).then().statusCode(status.value()).extract().response().jsonPath();
+	}
+
+	private JsonPath getRequestForInterview(long id) {
+		return given().when().get(basePath + INTERVIEW_PATH + "/" + id).then().statusCode(HttpStatus.OK.value()).log()
+				.everything().extract().jsonPath();
 	}
 
 	private Person buildPerson(String firstName, String lastName, String email, String title, PersonType type) {
-		return Person.builder().firstName(firstName).lastName(lastName).email(email).title(title)
-				.personType(type).business(mockBusiness).build();
+		return Person.builder().firstName(firstName).lastName(lastName).email(email).title(title).personType(type)
+				.business(business).build();
 	}
-	
-	private Interview createMockInterview() {
-		return Interview.builder()
-				.candidate(mockCandidate)
-				.date(mockDate)
-				.role(mockRole)
-				.interviewers(mockInterviewers)
-				.questions(mockQuestions)
-				.feedback(mockFeedback)
-				.id(mockId)
-				.build();
+
+	private Interview createInterview() {
+		return getInterviewBuilder().interviewers(new HashSet<Person>(Arrays.asList(firstInterviewer, secondInterviewer)))
+				.questions(createQuestions("Question 1", "Question 2", "Question 3")).feedback(feedback).build();
 	}
-	
-	private Question createMockQuestion(String question) {
-		return Question.builder()
-				.question(question)
-				.answer("Answer")
-				.id((long)(Math.random()*100))
-				.quality(Quality.Excellent)
-				.comment("Comment")
-				.build();
+
+	private Interview initBasicInterview() {
+		return getInterviewBuilder().build();
+	}
+
+	private InterviewBuilder getInterviewBuilder() {
+		return Interview.builder().candidate(candidate).role(role).date(createDate());
+	}
+
+	private Set<Question> createQuestions(String... questions) {
+		return Arrays.stream(questions).map(this::createQuestion).collect(Collectors.toSet());
+	}
+
+	private Question createQuestion(String question) {
+		return Question.builder().question(question).answer("Answer")
+				.quality(Quality.Excellent).comment("Comment").build();
+	}
+
+	private long createDate() {
+		return LocalDate.of(2017, Month.JANUARY, 25).toEpochDay();
 	}
 }
