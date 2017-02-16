654fdc7fc42352f53d90d12aa63b49c8691f263a
Armando Reyna <armando.i.reyna@gmail.com>
2017-02-16 16:32:35 -0700

    interviews completed and integration tests working

--- src/main/java/com/insys/trapps/respositories/interview/InterviewRepository.java
+++ src/main/java/com/insys/trapps/respositories/interview/InterviewRepository.java
@@ -1,9 +1,10 @@
 package com.insys.trapps.respositories.interview;
 
+import com.insys.trapps.model.interview.InterviewInlineProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.insys.trapps.model.interview.Interview;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = InterviewInlineProjection.class)
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
 }
